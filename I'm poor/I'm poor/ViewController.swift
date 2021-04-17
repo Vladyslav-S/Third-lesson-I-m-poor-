fc38db616b75e55ffcd040fde739a2e8df714de1
Vladyslav <savonikvladyslav@gmail.com>
2021-04-18 02:24:31 +0300

    Applecation I'm poor in my interpretation

---
+++ I'm poor/I'm poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I'm poor
+//
+//  Created by MACsimus on 18.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
